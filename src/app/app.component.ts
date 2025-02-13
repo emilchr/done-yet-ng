74c03d64be9d8ca2e67b30767474003d03b3e6b4
emilchr <emilchr@emilchr.com>
2025-02-13 15:57:48 +0100

    Fixing queue system for toasts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,16 @@ import { Component, inject } from '@angular/core';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { UserInputComponent } from './components/user-input/user-input.component';
 import { ToastService } from './service/toast.service';
+import { ToastComponent } from './toast/toast.component';
 
 @Component({
   selector: 'app-root',
-  imports: [TodoListComponent, UserInputComponent, NgOptimizedImage],
+  imports: [
+    TodoListComponent,
+    UserInputComponent,
+    NgOptimizedImage,
+    ToastComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/service/toast.service.ts
+++ src/app/service/toast.service.ts
@@ -6,14 +6,48 @@ import { Injectable } from '@angular/core';
 export class ToastService {
   currentToast: string[] = [];
   toastHistory: string[] = [];
+
+  enque(toast: string) {
+    if (this.currentToast.length > 0) {
+      this.dequeue();
+    }
+    this.currentToast.push(toast);
+  }
+  dequeue() {
+    if (!this.currentToast.length) {
+      if (!this.toastHistory.length) {
+        console.log('Queue empty');
+        return null;
+      }
+    }
+    return this.currentToast.pop();
+  }
+  // toastType = {
+  //   add: 'New todo added!',
+  //   remove: 'Undone!',
+  //   complete: 'Completed!',
+  // };
+
   showToast(message: any) {
-    this.currentToast.pop();
-    console.log(message);
-    // Make a queue?
-    this.currentToast.push(message);
-    this.toastHistory.push(message);
-    setTimeout(() => {
-      this.currentToast.pop();
-    }, 3000);
+    const toastTimeout = setTimeout(() => {
+      this.dequeue();
+    }, 1500);
+
+    this.enque(message);
+    // this.currentToast.push(message);
+    // console.log(message);
+    this.toastHistory.unshift(message);
+    //
+    // if (this.toastHistory.length > 0) {
+    //   clearTimeout(toastTimeout);
+
+    //   setTimeout(() => {
+    //     this.dequeue();
+    //   }, 1500);
+    // }
+    toastTimeout;
+
+    console.log('currentToast: ' + this.currentToast);
+    console.log('toastHistory: ' + this.toastHistory);
   }
 }
