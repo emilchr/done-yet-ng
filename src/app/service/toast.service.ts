81f04470d43a825976f0b6fbd4fa4a52f536c5dc
emilchr <emilchr@emilchr.com>
2025-02-18 13:11:27 +0100

    Refactored the queue to be an observable

--- src/app/service/toast.service.ts
+++ src/app/service/toast.service.ts
@@ -1,3 +1,4 @@
+// toast.service.ts
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
@@ -5,32 +6,35 @@ export interface Toast {
   id: number;
   message: string;
 }
+
 @Injectable({
   providedIn: 'root',
 })
 export class ToastService {
-  // toastObservable = new BehaviorSubject('');
+  private toastQueueSubject = new BehaviorSubject<Toast[]>([]);
+  toastQueue = this.toastQueueSubject.asObservable(); // Initialize as observable
 
-  toastQueue: Toast[] = [];
-  enqueue = (message: string) => {
-    const toastObject = {
-      id: Math.floor(Math.random() * 100),
+  enqueue(message: string) {
+    const toastObject: Toast = {
+      id: Math.floor(Math.random() * 100), // Apply random id to toast
       message: message,
     };
-    this.toastQueue.unshift(toastObject);
-  };
-  dequeue() {
+
+    const currentQueue = this.toastQueueSubject.getValue(); // Get the value from toastQueueSubject
+    this.toastQueueSubject.next([toastObject, ...currentQueue]); // Add to the front of the queue
+    this.dequeue(toastObject.id); // Dequeue after timeout
+  }
+
+  private dequeue(id: number) {
     setTimeout(() => {
-      // this.toastObservable.next(this.toastQueue[0].message);
-      this.toastQueue.pop();
-    }, 3000);
+      // sets delay for the queue process
+      const currentQueue = this.toastQueueSubject.getValue(); // Get the value from toastQueueSubject
+      const updatedQueue = currentQueue.filter((toast) => toast.id !== id); // Finds the correct id
+      this.toastQueueSubject.next(updatedQueue); // Updates toastQueueSubject with the new toast
+    }, 2000);
   }
 
   showToast(message: string) {
     this.enqueue(message);
-
-    if (this.toastQueue.length > 0) {
-      this.dequeue();
-    }
   }
 }
--- src/app/toast/toast.component.ts
+++ src/app/toast/toast.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { ToastService } from '../service/toast.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Toast, ToastService } from '../service/toast.service';
 
 @Component({
   selector: 'app-toast',
-  imports: [],
   templateUrl: './toast.component.html',
-  styleUrl: './toast.component.css',
+  imports: [],
+  styleUrls: ['./toast.component.css'],
 })
-export class ToastComponent implements OnInit {
-  constructor(private toastService: ToastService) {
-    this.toastQueue = this.toastService.toastQueue;
-    this.toastDisplay = '';
-  }
-  toastQueue: any;
-  toastDisplay: string;
+export class ToastComponent implements OnInit, OnDestroy {
+  toastQueue: Toast[] = []; // Correct type
+  private toastSubscription: Subscription | undefined;
+
+  constructor(private toastService: ToastService) {}
+
   ngOnInit() {
-    //   this.toastService.toastObservable.subscribe((toast: string) => {
-    //     this.toastDisplay = toast;
-    //   });
+    // Subscribes to the observable data stream
+    this.toastSubscription = this.toastService.toastQueue.subscribe((queue) => {
+      this.toastQueue = queue;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.toastSubscription) {
+      this.toastSubscription.unsubscribe(); // Unsubscribe to avoid memory leak
+    }
   }
 }
