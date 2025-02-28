e6c236000765607b63381c09772fb5d3332d7640
emilchr <emilchr@emilchr.com>
2025-02-28 12:55:00 +0100

    Started on tests.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -14,16 +15,30 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'done-yet-angular' title`, () => {
+  it(`should have the variable todos`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('done-yet-angular');
+    expect(app.todos).toEqual([]);
   });
 
-  it('should render title', () => {
+  it('should render the first todo item with "Removing the wet weather"', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, done-yet-angular');
+    expect(compiled.querySelector('span')?.textContent).toContain(
+      'Removing the wet weather'
+    );
+  });
+  it('should have a form for user with class "userInput"', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('.userInput'))).toBeTruthy();
+  });
+  it('should have a input field of type text', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    expect(
+      fixture.debugElement.query(By.css('input[name="userInput"]'))
+    ).toBeTruthy();
   });
 });
